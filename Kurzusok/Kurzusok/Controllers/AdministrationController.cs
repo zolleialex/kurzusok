9cacb832ec733bfa8c3123e647605fd4aa0d5eed
vaszabii <47735927+vaszabii@users.noreply.github.com>
2021-10-08 13:09:40 +0200

    Roles works completely

--- Kurzusok/Kurzusok/Controllers/AdministrationController.cs
+++ Kurzusok/Kurzusok/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
 ﻿using Kurzusok.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Kurzusok.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
