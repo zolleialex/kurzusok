3d515996edb1c1410670965eab14d66f7a4ddfb3
zolleialex <47735992+zolleialex@users.noreply.github.com>
2021-10-13 20:21:01 +0200

    Full database

--- Insert_Roles.sql
+++
@@ -1,6 +0,0 @@
-USE [Kurzusok]
-GO
-INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'27f39d9f-91dd-4d30-b278-05469b9286e9', N'Admin', N'ADMIN', N'a67cc832-e099-454a-8a4e-5392d477bed6')
-GO
-INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'aab1615a-a81f-467d-9c7d-d37b00398195', N'Felhasználó', N'FELHASZNÁLÓ', N'39def2c3-e7e9-4660-952b-3b8508070299')
-GO
