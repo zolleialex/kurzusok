44cd19a0451e7911cad3c4c2a8c22ab037d30e46
zolleialex <47735992+zolleialex@users.noreply.github.com>
2021-10-20 16:39:29 +0200

    full home display part 1

--- Kurzusok/Kurzusok/Controllers/TeachersController.cs
+++ Kurzusok/Kurzusok/Controllers/TeachersController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Kurzusok.Data;
 using Kurzusok.Models;
 
 namespace Kurzusok.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class TeachersController : Controller
     {
         private readonly KurzusokContext _context;
