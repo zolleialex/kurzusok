366d767d2f43abfcce8c911ba5adc67b711a10a9
zolleialex <47735992+zolleialex@users.noreply.github.com>
2021-12-05 14:42:59 +0100

    Bar chart statistic

--- Kurzusok/Kurzusok/Controllers/TeachersController.cs
+++ Kurzusok/Kurzusok/Controllers/TeachersController.cs
@@ -8,6 +8,7 @@ using Kurzusok.Models;
 using Kurzusok.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Kurzusok.Controllers
 {
@@ -26,22 +27,30 @@ namespace Kurzusok.Controllers
         }
 
         [Route("Teachers")]
-        public async Task<IActionResult> Index(string search)
+        public async Task<IActionResult> Index(bool justData, string search)
         {
             _teacherViewModel.LastTwoSemester = await _context.Semester.OrderByDescending(b => b.Id).Take(2).ToListAsync();
             _teacherViewModel.LastSemesterWeek = _teacherViewModel.LastTwoSemester.Where(b => b.Id == _teacherViewModel.LastTwoSemester.Max(b => b.Id)).Select(b => b.Weeks).FirstOrDefault();
-            _teacherViewModel.Positions = await _context.Positions.ToListAsync();        
-            List<Teachers> teachers= new List<Teachers>();
+            _teacherViewModel.Positions = await _context.Positions.ToListAsync();
+            List<Teachers> teachers = new List<Teachers>();
             if (!string.IsNullOrEmpty(search))
             {
-                teachers = await _context.Teachers.Where(b => b.Name.Contains(search)).OrderByDescending(b => b.IsWorking).Include(b => b.Position).Include(b => b.CoursesLink).ThenInclude(c=>c.Course).ThenInclude(b=>b.Subject).ThenInclude(k=>k.Semester).Include(b => b.CoursesLink).ThenInclude(c => c.Course).ThenInclude(b => b.Subject).ThenInclude(v=>v.ProgrammesLink).ThenInclude(i=>i.Programme).ToListAsync();
+                teachers = await _context.Teachers.Where(b => b.Name.Contains(search)).OrderByDescending(b => b.IsWorking).Include(b => b.Position).Include(b => b.CoursesLink).ThenInclude(c => c.Course).ThenInclude(b => b.Subject).ThenInclude(k => k.Semester).Include(b => b.CoursesLink).ThenInclude(c => c.Course).ThenInclude(b => b.Subject).ThenInclude(v => v.ProgrammesLink).ThenInclude(i => i.Programme).ToListAsync();
             }
             else
             {
                 teachers = await _context.Teachers.OrderByDescending(b => b.IsWorking).Include(b => b.Position).Include(b => b.CoursesLink).ThenInclude(c => c.Course).ThenInclude(b => b.Subject).ThenInclude(k => k.Semester).Include(b => b.CoursesLink).ThenInclude(c => c.Course).ThenInclude(b => b.Subject).ThenInclude(v => v.ProgrammesLink).ThenInclude(i => i.Programme).ToListAsync();
             }
             _teacherViewModel.TeachersList = teachers;
-            return View(_teacherViewModel);
+            if (justData != true)
+            {
+                return View(_teacherViewModel);
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+
         }
 
         // GET: Teachers/Create
@@ -189,9 +198,72 @@ namespace Kurzusok.Controllers
 
         public async Task<IActionResult> NextPosition(int posId)
         {
-            string currentposName = await _context.Positions.Where(b => b.PositionId == posId).Select(b=>b.PositionName).FirstOrDefaultAsync();
-            int currentposWeek = await _context.Positions.Where(b => b.PositionId == posId).Select(b => b.Hoursperweek).FirstOrDefaultAsync();
-            return Json(new { currentposName, currentposWeek });
+            await Index(true, null);
+            var pos = await _context.Positions.Where(b => b.PositionId == posId).FirstOrDefaultAsync();
+            string currentposName = pos.PositionName;
+            int currentposWeek = pos.Hoursperweek;
+            string visualdata = "[";
+            bool notNull = false;
+            foreach (Teachers teacher in _teacherViewModel.TeachersList)
+            {
+                if (teacher.IsWorking && teacher.PositionId == posId)
+                {
+                    notNull = true;
+                    double? fullhours = 0;
+                    double? parthours = 0;
+                    double? subjecthours = 0;
+                    foreach (var courselink in teacher.CoursesLink)
+                    {
+                        if (courselink.Course.Subject.Semester.Id == _teacherViewModel.LastTwoSemester[0].Id && (courselink.Course.CourseType == "Gyakorlat" || courselink.Course.CourseType == "Elmélet" || courselink.Course.CourseType == "Labor"))
+                        {
+                            foreach (var programme in courselink.Course.Subject.ProgrammesLink)
+                            {
+
+                                if (programme.Programme.Training == "nappali")
+                                {
+                                    if (courselink.Course.CourseType == "Gyakorlat")
+                                    {
+                                        subjecthours = courselink.Course.Subject.GyHours;
+                                    }
+                                    else if (courselink.Course.CourseType == "Elmélet")
+                                    {
+                                        subjecthours = courselink.Course.Subject.EHours;
+                                    }
+                                    else
+                                    {
+                                        subjecthours = courselink.Course.Subject.LHours;
+                                    }
+
+                                    fullhours += Convert.ToDouble(courselink.Loads) / 100 * subjecthours;
+                                }
+                                else
+                                {
+                                    double semWeek = _teacherViewModel.LastTwoSemester[0].Weeks;
+                                    subjecthours = courselink.Course.Subject.CorrespondHours;
+                                    parthours += Convert.ToDouble(courselink.Loads) / 100 * (subjecthours/semWeek);
+                                }
+                            }
+
+                        }
+                    }
+                    string fullhoursStr = fullhours.ToString();
+                    fullhoursStr=fullhoursStr.Replace(',', '.');
+                    string parthoursStr = parthours.ToString();
+                    parthoursStr = parthoursStr.Replace(',', '.');
+                    visualdata += $"[\"{teacher.Name}\", {fullhoursStr},{parthoursStr}],";
+                }
+            }
+            if (notNull)
+            {
+                visualdata = visualdata.Remove(visualdata.Length - 1, 1);
+                visualdata += "]";
+                return Json(new {isvalid=true, currentposName, currentposWeek, visualdata });
+            }
+            else
+            {
+                return Json(new { isvalid=false ,response = "A "+currentposName+" munkakörhöz nincs oktató rendelve." });
+            }
+
         }
     }
 }
