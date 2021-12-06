83ee9041c2c2527e6dd969441e49c7be6d542d33
zolleialex <47735992+zolleialex@users.noreply.github.com>
2021-12-06 21:28:00 +0100

    Modal changes

--- Kurzusok/Kurzusok/Models/Courses.cs
+++ Kurzusok/Kurzusok/Models/Courses.cs
@@ -11,14 +11,14 @@ namespace Kurzusok.Models
         public int CourseId { get; set; }
         public int SubjectId { get; set; }
         public string CourseType { get; set; }
-        [Range(0, 1000, ErrorMessage = "Az létszám csak 0 és 1000 között egész érték lehet!")]
+        [Range(0, 1000, ErrorMessage = "A létszám csak 0 és 1000 között egész érték lehet!")]
         public int Members { get; set; }
         public string Classroom { get; set; }
         public string Comment { get; set; }
         public bool NeptunOk { get; set; }
         public string Software { get; set; }
-        [Required(ErrorMessage = "Az kurzus kódjának megadása kötelező!")]
-        [Range(0, 100, ErrorMessage = "A kurzus kódja 0 és 100 között egész érték lehet!")]
+        [Required(ErrorMessage = "A kurzus kódjának megadása kötelező!")]
+        [Range(1, 100, ErrorMessage = "A kurzus kódja 1 és 100 között egész érték lehet!")]
         public int CourseCode { get; set; }
 
         public virtual Subjects Subject { get; set; }
